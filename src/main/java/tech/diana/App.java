e89bb25b3d25fa1dd8cc588978f96c3d99bac740
diana <dianakieru1@gmil.com>
2022-05-02 03:47:09 +0300

    added heroList data

--- src/main/java/tech/diana/App.java
+++ src/main/java/tech/diana/App.java
@@ -2,23 +2,52 @@ package tech.diana;
 
 import spark.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import spark.template.handlebars.HandlebarsTemplateEngine;
+import tech.diana.domain.Hero;
+import tech.diana.domain.enumerations.Strength;
+import tech.diana.domain.enumerations.Weaknesses;
 
 
 import static spark.Spark.*;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
+        Hero hero = new Hero();
+        hero.setName("Diana");
+        hero.setAge(25);
+        hero.setStrength(Strength.A);
+        hero.setWeakness(Weaknesses.F);
+
+        Hero hero1 =new Hero();
+        hero1.setName("Kieru");
+        hero1.setAge(23);
+        hero1.setStrength(Strength.B);
+        hero1.setWeakness(Weaknesses.G);
+
+        Hero hero2 = new Hero();
+        hero2.setName("Maureen");
+        hero2.setAge(24);
+        hero2.setStrength(Strength.B);
+        hero2.setWeakness(Weaknesses.H);
+
+        List<Hero> heroList = new ArrayList<>();
+        heroList.add(hero);
+        heroList.add(hero1);
+        heroList.add(hero2);
+
+
         get("/hello", (req, res) -> {
-            Map<String, String> model = new HashMap<>();
+            Map<String, Object> model = new HashMap<>();
+            model.put("heroList", heroList);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
 
 
     }
