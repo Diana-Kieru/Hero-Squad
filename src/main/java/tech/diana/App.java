d02beeb1d0eb259d291781ec40ecef89279e0f10
diana <dianakieru1@gmil.com>
2022-05-03 22:13:55 +0300

    added index.hbs form

--- src/main/java/tech/diana/App.java
+++ src/main/java/tech/diana/App.java
@@ -18,10 +18,11 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        Hero hero = new Hero("Diana", 25, Strength.A,Weaknesses.F);
+        staticFileLocation("/public");
+        Hero hero = new Hero("Diana", 25, Strength.A, Weaknesses.F);
 
 
-        Hero hero1 =new Hero("Kieru", 23, Strength.B, Weaknesses.F) ;
+        Hero hero1 = new Hero("Kieru", 23, Strength.B, Weaknesses.F);
 
 
         Hero hero2 = new Hero("Maureen", 24, Strength.C, Weaknesses.H);
@@ -32,13 +33,26 @@ public class App {
         heroList.add(hero1);
         heroList.add(hero2);
 
-
+        Map<String, Object> model = new HashMap<>();
         get("/hello", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
+
             model.put("heroList", heroList);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        post("heroes", (req, res) -> {
+            String name = req.queryParams("name");
+            int age = Integer.parseInt(req.queryParams("age"));
+            Strength strength = Strength.valueOf(req.queryParams("strength"));
+            Weaknesses weakness = Weaknesses.valueOf(req.queryParams("weakness"));
+            Hero newhero = new Hero(name, age, strength, weakness);
+            heroList.add(newhero);
+            model.put("heroList", heroList);
+
+
+            res.redirect("/hello");
+            return null;
+        }, new HandlebarsTemplateEngine());
 
 
     }
